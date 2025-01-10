8d90c6c989863257fec4ef815271f65bfec3876b
jonz94 <jody16888@gmail.com>
2025-01-10 15:52:08 +0800

    docs: disable pre-rendering

--- app/no-ssr.tsx
+++ app/no-ssr.tsx
@@ -2,4 +2,6 @@
 
 import dynamic from 'next/dynamic'
 
+// NOTE: disable pre-rendering is necessary to avoid `window is not defined` error
+// docs: https://nextjs.org/docs/app/building-your-application/optimizing/lazy-loading#skipping-ssr
 export const ContentWithNoSSR = dynamic(() => import('@/app/content').then((mode) => mode.Content), { ssr: false })
